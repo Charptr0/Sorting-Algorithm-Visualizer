76911d0b99f6cb291f8a2246f354959c9c3a202a
Charptr0 <CharlesOfficial1@hotmail.com>
2021-04-15 23:15:28 -0400

    added length, height and display()

--- Rect_Block/RectBlock.h
+++ Rect_Block/RectBlock.h
@@ -12,19 +12,26 @@ class RectBlock
     private:
 
         float position[2];
+        float size_multiplier;
+        float length;
+        float height;
+
         sf::Color color = sf::Color::White;
-        float size;
         sf::RectangleShape block;
 
         void setBlockPosition();
         void setBlockColor();
+        void resizeBlock();
 
 
     public:
-        RectBlock(const float &positionX, const float &positionY, const float &size);
+
+        RectBlock(const float &length, const float &height, const float &positionX, const float &positionY, const float &size_multiplier = 1);
         void setPosition(const float &positionX, const float &positionY);
+        void resize(const float &length, const float &height);
         float getPositionX();
         float getPositionY();
         float getSize();
+        sf::RectangleShape display();
 
 };
