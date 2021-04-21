4e438c2e117a1807f3515cccc911117664d51f23
Charptr0 <CharlesOfficial1@hotmail.com>
2021-04-20 21:36:07 -0400

    added comments

--- Text_Manager/text.cpp
+++ Text_Manager/text.cpp
@@ -4,21 +4,25 @@ const std::string FONT_FILE_PATH = "Text_Manager/Fonts/arial.ttf";
 
 Text::Text(const std::string &string, const float &posX, const float &posY, const float &font_size)
 {
-    if(!this->font.loadFromFile(FONT_FILE_PATH)) {std::cout << "An error has occurred while loading the font\n";}
+    if(!this->font.loadFromFile(FONT_FILE_PATH)) {std::cout << "An error has occurred while loading the font\n";} //load arial font
 
-    this->position_x = posX;
+    //set the position of the text
+    this->position_x = posX; 
     this->position_y = posY;
+    //set the position of the text
 
-    this->text.setFillColor(sf::Color::White);
-    this->text.setFont(this->font);
-    this->text.setString(string);
-    this->text.setPosition(sf::Vector2f(this->position_x, this->position_y));
-    this->text.setScale(sf::Vector2f(font_size, font_size));
+
+    this->text.setFillColor(sf::Color::White); //set the color of the text to white
+    this->text.setFont(this->font); //set the font
+    this->text.setString(string); //set the text
+    this->text.setPosition(sf::Vector2f(this->position_x, this->position_y)); //set the position
+    this->text.setScale(sf::Vector2f(font_size, font_size)); //set the size
 }
 
 Text::Text()
 {
-    if(!this->font.loadFromFile(FONT_FILE_PATH)) {std::cout << "An error has occurred while loading the font\n";}
+    if(!this->font.loadFromFile(FONT_FILE_PATH)) {std::cout << "An error has occurred while loading the font\n";} //load arial font
+    
     this->text.setFillColor(sf::Color::White);
     this->text.setFont(this->font);
     this->text.setString("");
--- Text_Manager/text.h
+++ Text_Manager/text.h
@@ -8,20 +8,21 @@
 class Text
 {
     private:
-        sf::Font font;
+        sf::Font font; //the default font is arial
         sf::Text text;
         float position_x;
         float position_y;
 
     public:
         Text(const std::string &string, const float &posX, const float &posY, const float &font_size = 1);
+        
+        //empty constructor
         Text();
 
         void updateText(const std::string &new_string);
 
+        //use this function for screen.draw()
         sf::Text getText();
-
-        friend Text generateComparsionText(const int &number_of_comparsions);
 
 };
 
